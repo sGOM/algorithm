487069b304c2ba4ff4581de33325a30867558939
sGOM <pooh6195@naver.com>
2022-09-23 13:33:50 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -4,17 +4,17 @@ class Solution {
     public int[] solution(int n, String[] words) {
         
         HashSet<String> set = new HashSet<String>(words.length);
-        String startWith = words[0].substring(0, 1);
+        String startWith = words[0].substring(words[0].length() - 1, words[0].length());
+        set.add(words[0]);
 
-        for (int i = 0; i < words.length; i++) {
+        for (int i = 1; i < words.length; i++) {
             if (set.contains(words[i]) || !words[i].startsWith(startWith)) {
-                int num     = (i + 1) % n;
-                int turn    = ((i + 1) / n) + 1;
-                if ((i + 1) % n == 0) { num = n; turn--; }
+                int num     = i % n + 1;
+                int turn    = i / n + 1;
                 return new int[] { num, turn };
             }
-            set.add(words[i]);
             startWith = words[i].substring(words[i].length() - 1, words[i].length());
+            set.add(words[i]);
         }
 
         return new int[] { 0, 0 };
