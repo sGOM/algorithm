8edf5aa40eb94427ea0873b982d66c172a8aa92d
sGOM <pooh6195@naver.com>
2022-09-23 14:02:51 +0900

    [level 2] Title: 짝지어 제거하기, Time: 56.42 ms, Memory: 61 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : s.toCharArray()) {
+            if (stack.empty()) { stack.push(c); continue; }
+            if (stack.peek() == c)  stack.pop();
+            else                    stack.push(c);
+        }
+
+        return stack.empty() ? 1 : 0;
+    }
+}
