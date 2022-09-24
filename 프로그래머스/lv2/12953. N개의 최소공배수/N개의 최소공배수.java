457d7f1fe41c7e268bec4cbaf2e8ea5fa8ae65b5
sGOM <pooh6195@naver.com>
2022-09-24 15:54:04 +0900

    [level 2] Title: N개의 최소공배수, Time: 1.20 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -2,12 +2,11 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] arr) {
-        Arrays.sort(arr);
         boolean isCommon    = true;
-        int     answer      = arr[arr.length - 1];
+        int     answer      = Arrays.stream(arr).max().getAsInt();
         
-        for (int i = arr.length - 1; 0 <= i; i--) {
-            int GCD     = arr[i];
+        for (int el : arr) {
+            int GCD     = el;
             int temp1   = answer;
             int temp2   = 0;
             while (temp1 % GCD != 0) {
@@ -15,7 +14,7 @@ class Solution {
                 temp1   = GCD;
                 GCD     = temp2;
             }
-            answer = answer * (arr[i] / GCD);
+            answer = answer * (el / GCD);
         } 
         
         return answer;
