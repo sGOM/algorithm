123c9959ee46b6a6a8b047955887504b58f17e29
sGOM <pooh6195@naver.com>
2022-09-28 17:23:20 +0900

    주석 추가, 유지-보수가 쉽게 변환

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,30 +2,40 @@ import java.util.PriorityQueue;
 
 class Solution {
     public int[] solution(String[] operations) {
+        // 오름차순 PriorityQueue
         PriorityQueue<Integer> minPriQueue = new PriorityQueue<>();
+        // 내림차순 PriorityQueue
         PriorityQueue<Integer> maxPriQueue = new PriorityQueue<>(11, (a, b) -> b - a);
 
-        String opRemoveMax = "D 1";
-        String opRemoveMin = "D -1";
-
+        // 명령어 배열을 순회
         for (String op : operations) {
-            if (op.equals(opRemoveMax)) {
-                if (minPriQueue.size() != 0) {
-                    minPriQueue.remove(maxPriQueue.poll());
-                }
-                continue;
+            // 명령어 유형에 따라 실행
+            switch (op.charAt(0)) {
+                // 삭제 명령인 경우
+                case 'D' :
+                    // PriorityQueue의 크기가 0이면 아무것도 안함
+                    if(minPriQueue.size() == 0) break;
+                    switch (Integer.parseInt(op.substring(2))) {
+                        // 최소값 제거
+                        case -1 :
+                            maxPriQueue.remove(minPriQueue.poll()); break;
+                        // 최대값 제거
+                        case  1 :
+                            minPriQueue.remove(maxPriQueue.poll()); break;
+                    }
+                    break;
+                // 삽입 명령인 경우
+                case 'I' :
+                    // 2가지 PriorityQueue에 명령어가 준 숫자 추가
+                    minPriQueue.add(Integer.parseInt(op.substring(2)));
+                    maxPriQueue.add(Integer.parseInt(op.substring(2)));
+                    break;
             }
-            if (op.equals(opRemoveMin)) {
-                if (maxPriQueue.size() != 0) {
-                    maxPriQueue.remove(minPriQueue.poll());
-                }
-                continue;
-            }
-            minPriQueue.add(Integer.parseInt(op.substring(2)));
-            maxPriQueue.add(Integer.parseInt(op.substring(2)));
         }
 
+        // 명령어 실행이 끝난 후 PriorityQueue의 크기가 0이면 [0, 0] 반환
         if (minPriQueue.size() == 0) return new int[] {0, 0};
+        // 그렇지 않다면 [최대값, 최소값] 반환
         return new int[] {maxPriQueue.peek(), minPriQueue.peek()};
     }
 }
