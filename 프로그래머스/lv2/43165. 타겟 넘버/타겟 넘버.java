5e7b8d900b82fa247bbc9af9e975b767986fbb0e
sGOM <pooh6195@naver.com>
2022-10-09 15:53:31 +0900

    DFS 메서드 리팩토링

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -7,12 +7,8 @@ class Solution {
     public int DFS(int[] numbers, int target, int sum, int idx) {
         // 끝까지 탐색한 경우 target == sum 이면 1을 아니면 0을 반환
         if (idx == numbers.length) return (sum == target) ? 1 : 0;
-        
-        int cnt = 0;
-        // +를 한경우
-        cnt += DFS(numbers, target, sum + numbers[idx], idx + 1);
-        // -를 한경우
-        cnt += DFS(numbers, target, sum - numbers[idx], idx + 1);
-        return cnt;
+      
+        // (+를 한경우) + (-를 한경우)
+        return DFS(numbers, target, sum + numbers[idx], idx + 1) + DFS(numbers, target, sum - numbers[idx], idx + 1);
     }
 }
