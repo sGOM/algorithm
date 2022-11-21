18931269b47014651ac7168bae2d717226462792
Heo SeokJin <pooh6195@naver.com>
2022-11-21 16:27:33 +0900

    [level 2] Title: 위장, Time: 6.08 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/README.md
+++ 프로그래머스/lv2/42578. 위장/README.md
@@ -1,10 +1,10 @@
 # [level 2] 위장 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
 
 ### 성능 요약
 
-메모리: 77.4 MB, 시간: 0.06 ms
+메모리: 76.1 MB, 시간: 6.08 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42578. 위장/위장.java
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -1,25 +1,20 @@
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 class Solution {
     public int solution(String[][] clothes) {
-        // 옷의 종류 별로 카운팅할 HashMap 선언
-        HashMap<String, Integer> kindClothes = new HashMap<>();
-        // 주어진 clothes를 순회하면서 옷 종류 카운팅
-        for (String[] c : clothes) {
-            // 이미 있는 옷 종류면 기존에 + 1
-            if (kindClothes.containsKey(c[1]))  kindClothes.put(c[1], kindClothes.get(c[1]) + 1);
-            // 새로운 옷 종류면 value를 1로 하고 추가
-            else                                kindClothes.put(c[1], 1);
-        }
-        
-        int answer = 1;
-        // (이전 경우의 수) *= (해당 종류의 수 + 해당 종류를 안입는 경우(==1))
-        for (Integer num : kindClothes.values()) 
-            answer *= (num + 1);
-
-        // 모든 종류의 옷을 안 입는 경우는 빼줌
-        answer--;
-        
-        return answer;
+        return Arrays.stream(clothes)
+                // clothes[0]~clothes[clothes.length-1] 각각 역시 배열이므로 각 배열에서 원하는 값인 1번째 값만 추출
+                .map(cloth -> cloth[1])
+                // groupingBy 라는 놀라운 기능으로 key 는 위에서 추출한 값 그대로, value 는 Collectors 에서 지원하는 counting 메서드를 사용
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                // 위에서 반환한 Map 을 value 값으로만 이뤄진 Collection 으로 변환하고 해당 Collection 의 stream 을 생성
+                .values().stream()
+                // Collectors.counting() 의 반환 값이 Long 이 었으므로 윗줄에서 생성된 stream 이 Long 타입임, Integer 로 변환
+                .map(Long::intValue)
+                // 초기값을 1로 설정하고 (각 요소 + 1)을 이전 결과 값에 곱하고 반환한후에
+                // 반환값에 최종적으로 -1 을 하고 return
+                .reduce(1, (a, b) -> a * (b + 1)) - 1;
     }
 }
