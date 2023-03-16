8ae5ceb33c20ec147a6f0b3a4a55cb338a763dc0
Heo SeokJin <pooh6195@naver.com>
2023-03-17 06:55:35 +0900

    [unrated] Title: 귤 고르기, Time: 40.75 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        // 귤들의 갯수를 카운팅
+        // 어떤 [크기]가 배열의 원소로 들어있을지 모르니 Hash를 사용하는 것이 효율적임
+        HashMap<Integer, Integer> tangerineSizeBox = new HashMap<>();
+        for (int t : tangerine) {
+            if (tangerineSizeBox.containsKey(t)) {
+                tangerineSizeBox.put(t, tangerineSizeBox.get(t) + 1);
+            } else {
+                tangerineSizeBox.put(t, 1);
+            }
+        }
+        
+        // 집계한 [크기]별 귤들의 갯수를 내림차순으로 정렬
+        List<Integer> quantityList = new ArrayList<>(tangerineSizeBox.values());
+        Collections.sort(quantityList, Collections.reverseOrder());
+        
+        int answer = 0;
+        
+        // 상자에 가장 많은 갯수가 존재하는 [크기]의 귤들부터 넣고 다 채웠으면 종류의 수 반환
+        for (int quantity : quantityList) {
+            k -= quantity;
+            answer++;
+            if (k <= 0) return answer;
+        }
+        
+        return answer;
+    }
+}
