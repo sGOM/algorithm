4df4e23133e3c8c852a11903762561edf7e64e6d
Heo SeokJin <pooh6195@naver.com>
2023-05-19 03:04:16 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.06 ms, Memory: 52.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String solution(int n) {
+        // 정답을 저장할 변수
+        StringBuilder answer = new StringBuilder();
+        // 진법 정의
+        int[] scale = {1, 2, 4};
+        // 자릿수를 확인하기 위해서는 1을 빼야함
+        n -= 1;
+        
+        // 자릿수를 저장하는 변수
+        int cipher = 1;
+        // n의 자릿수 구하기
+        while (pow(scale.length, cipher) <= n) {
+            // n에서 cipher자릿수의 숫자 갯수만큼 빼기
+            n -= pow(scale.length, cipher); 
+            // 자릿수 + 1
+            cipher++;
+        }
+        
+        // 해당 시점에서 정답은, cipher 자릿수 숫자 중 n번째 자릿 수
+        
+        // 자릿수 만큼 순회하며 각 자릿수에 올 숫자 결정
+        for (; 0 < cipher; cipher--) {
+            int divider = pow(scale.length, cipher - 1);
+            answer.append(scale[n / divider]);
+            n %= divider;
+        }
+        
+        return answer.toString();
+    }
+    
+    // 제곱함수 이쁘게 뺌
+    public int pow(int base, int exp) {
+        return (int)Math.pow(base, exp);
+    }
+}
