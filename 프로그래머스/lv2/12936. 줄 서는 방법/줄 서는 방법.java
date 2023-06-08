e0b6cd0b6fa57cb1b76ed7e78ee4ff2dc6ee3188
Heo SeokJin <pooh6195@naver.com>
2023-06-09 01:15:14 +0900

    필요없는 코드 삭제

--- 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, long k) {
         int[] answer = new int[n];
-        int[] factorialList = new int[n];
         
         // 1 ~ n까지 순서대로 저장
         List<Integer> numbers = new ArrayList<>(n);
@@ -16,7 +15,7 @@ class Solution {
         // n 번 순회하며
         for (int i = 0; i < answer.length; i++) {
             // 정답 배열의 i 번째 수를 제외한 경우 수로 갱신
-            numOfcase /= (n - i);   // 3 -> 2! 1! 1
+            numOfcase /= (n - i);
             // O(n), n = numbers.size()
             // numbers의 (k / numOfCase)번째 수가 answer[i] 번째 수
             answer[i] = numbers.remove((int)(k / numOfcase));
@@ -33,17 +32,6 @@ class Solution {
         
         return res;
     }
-    
-    /*
-    public long factorial(int num, int[] factorialList) {
-        int idx = factorialList.length - 1;
-        factorialList[idx--] = 1;
-        for (int i = 1; 0 <= idx; i++, idx--) {
-            factorialList[idx] = factorialList[idx + 1] * i;
-        }
-        
-        return res;
-    }*/
     
     // 시간 복잡도
     // O(n^2), n = (일렬로 설 사람의 수)
