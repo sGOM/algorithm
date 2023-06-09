0d533182800bcdb32c4d0d658eac3f6b3528b395
Heo SeokJin <pooh6195@naver.com>
2023-06-10 06:21:26 +0900

    가독성 좋게 코드 변경 및 해설 추가
    속도는 조금 느려지지만, 가독성이 좋아지는 방향으로 코드를 수정
    각 코드 및 시간복잡도에 대한 해설 추가

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,56 +1,63 @@
 import java.util.*;
 
+// 용어 정의
+// 그룹 - 다리로 서로 오갈 수 있는 섬의 집합
 class Solution {
     public int solution(int n, int[][] costs) {
-        HashSet<Integer> bridgedIslands = new HashSet<>();
-        LinkedList<HashSet<Integer>> groups = new LinkedList<>();
+        LinkedList<HashSet<Integer>> groups = new LinkedList<>();       // 여러 그룹들을 저장
+        ArrayList<HashSet<Integer>> findedGroup = new ArrayList<>();    // 현재 건설을 고려하는 다리들의 그룹들
         int answer = 0;
         
         // 건설 비용 순으로 정렬
         Arrays.sort(costs, Comparator.comparingInt(c -> c[2]));
         
+        // 건설 비용 오름차순으로 모든 다리 후보를 순회
         for (int[] cost : costs) {
             int island1 = cost[0], island2 = cost[1];
             
-            // 두 섬 모두 아직 다리가 지어지지 않은 경우
-            if (!bridgedIslands.contains(island1) && !bridgedIslands.contains(island2)) {
-                HashSet<Integer> newGroup = new HashSet<>();
-                newGroup.add(island1); newGroup.add(island2);
-                groups.add(newGroup);
-                bridgedIslands.add(island1); bridgedIslands.add(island2);
-                answer += cost[2];
-            // 두 섬 중 한 섬만 다리가 지어진 경우
-            } else if (!bridgedIslands.contains(island1) == bridgedIslands.contains(island2)) {
-                for (HashSet<Integer> group : groups) {
-                    if (group.contains(island1) || group.contains(island2)) {
-                        group.add(island1); group.add(island2);
-                        bridgedIslands.add(island1); bridgedIslands.add(island2);
-                        answer += cost[2];
-                        break;
-                    }
-                }
-            // 두 섬 모두 다리가 지어진 경우
-            } else {
-                ArrayList<HashSet<Integer>> tempList = new ArrayList<>();
-                for (HashSet<Integer> group : groups) {
-                    if (group.contains(island1)) tempList.add(group);
-                    if (group.contains(island2)) tempList.add(group);
-                    if (tempList.size() == 2) break;
-                }
-                
-                HashSet<Integer> group1 = tempList.get(0);
-                HashSet<Integer> group2 = tempList.get(1);
-                // 두 섬이 다른 그룹에 속해 있다면
-                if (!group1.equals(group2)) {
-                    group1.addAll(group2);
-                    groups.remove(group2);
-                    answer += cost[2];
-                }
+            // 이미 다리가 지어진(그룹에 속해있는) 섬이라면, 해당 섬이 속한 그룹을 findedGroup에 추가 
+            for (HashSet<Integer> group : groups) {
+                if (group.contains(island1)) findedGroup.add(group);
+                if (group.contains(island2)) findedGroup.add(group);
+                if (findedGroup.size() == 2) break;
             }
             
-            if(bridgedIslands.size() == n && groups.size() == 1) break;
+            // 이미 그룹에 속한 섬의 갯수에 따라
+            switch (findedGroup.size()) {
+                // 두 섬 모두 그룹에 속하지 못한 경우 (다리가 하나도 안지어진 경우)
+                // 새로운 그룹 생성
+                case 0:
+                    groups.add(new HashSet<>(List.of(island1, island2)));
+                    answer += cost[2];
+                    break;
+                // 한 섬만 그룹에 속한 경우
+                // 그룹을 가지지 못한 섬을 해당 그룹에 추가
+                case 1:
+                    HashSet<Integer> group = findedGroup.get(0);
+                    group.addAll(List.of(island1, island2));
+                    answer += cost[2];
+                    break;
+                // 두 섬 모두 그룹에 속한 경우
+                case 2:
+                    HashSet<Integer> group1 = findedGroup.get(0);
+                    HashSet<Integer> group2 = findedGroup.get(1);
+                    // 이미 같은 그룹이 아니라면
+                    if (!group1.equals(group2)) {
+                        // 두 그룹을 합침
+                        group1.addAll(group2);
+                        groups.remove(group2);
+                        answer += cost[2];
+                    }
+                    break;
+            }
+            
+            // 모든 섬이 1개의 그룹이 되었다면 반복문 종료
+            if(groups.get(0).size() == n) break;
+            findedGroup.clear();
         }
         
         return answer;
-    }
+    }   
+    // 시간 복잡도
+    // O(n * k), n = costs.length, k = groups.length <= (섬 갯수)/2
 }
