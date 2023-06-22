80bf31cb2e9483dee2c7f02668216206ecd9e3a4
Heo SeokJin <pooh6195@naver.com>
2023-06-22 12:03:53 +0900

    더 좋은 풀이 주석으로 추가

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -31,3 +31,19 @@ class Solution {
     // 전체 시간복잡도
     // O(n), n = (storey 자릿수)
 }
+
+/* 더 깔끔한 풀이
+        int answer = 0;
+        while (storey != 0) {
+            int upperNumber = (storey %100)/10;
+            int number = storey % 10;
+            if (number > 5 || number == 5 && upperNumber>=5) {
+                storey += 10;
+                answer += (10 - number);
+            } else {
+                answer += number;
+            }
+            storey = storey / 10;
+        }  
+        return answer;
+*/
