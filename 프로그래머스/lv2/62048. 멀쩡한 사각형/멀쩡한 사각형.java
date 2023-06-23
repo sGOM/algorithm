3d2a1d3857ff7d8adf4315a335a75599c78ca8c6
Heo SeokJin <pooh6195@naver.com>
2023-06-23 11:47:38 +0900

    가독성 개선을 위한 코드 리팩토링

--- 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -1,13 +1,12 @@
 class Solution {
     public long solution(long w, long h) {
+        /*
         // (전체 정사각형) - (선이 격자점을 지나지 않는 경우 잘린 정사각형)
         long answer = (w * h) - (w + h - 1);
-        // 최대 공약수
-        long gcd = getGcd(w, h);
-        
-        // 나누어 떨어지지 않는다면, 선이 격자점을 지나지 않음
-        // 나누어 떨어진다면, 선이 격자점을 (최대공약수 - 1)번 통과하여 그만큼의 정사각형이 잘리지 않음
-        return gcd == 0 ? answer : answer + gcd - 1;
+        // 선이 격자점을 (최대공약수 - 1)번 통과하여 그만큼의 정사각형이 잘리지 않음
+        return answer + getGcd(w, h) - 1;
+        */
+        return (w * h) - (w + h) + getGcd(w, h);
     }
     
     // O(log n), n = min(a, b)
