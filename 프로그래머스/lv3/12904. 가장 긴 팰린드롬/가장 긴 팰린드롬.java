f407c8dc4d2725ff67b4903fed0cf90e782bb8a4
Heo SeokJin <pooh6195@naver.com>
2023-06-27 03:12:56 +0900

    시간 복잡도 분석 fix, 다른 간단한 방식 주석 추가

--- 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,8 +1,8 @@
-class Solution
-{
-    public int solution(String s)
-    {
-        // O(n^4), n = s.length()
+class Solution {
+    // 긴 문자열을 먼저 확인하고, palindrome을 발견하면 즉시 종료
+    // 중심을 잡고 확인하는 코드보다 반복은 더 많이 하지만 전체적인 연산횟수는 비슷하거나 더 적을 수 있다고 생각함, 이후 추가
+    public int solution(String s) {
+        // O(n^3), n = s.length()
         for (int len = s.length(); 0 < len; len--) {
             for (int left = 0, right = len - 1; right < s.length(); left++, right++) {
                 if (isPalindrome(s, left, right)) {
@@ -26,9 +26,49 @@ class Solution
     }
     
     // 시간 복잡도
-    // O(n^4), n = s.length()
-    // 이는 최악의 경우에만, 모든 부분 문자열이 palindrome이 아닐때
-    // 도출법 -> Σ(1 <= L <= n){ L * (n - L + 1) * L }
-    // (바깥쪽 for문) * (안쪽 for문) * (isPalindrome)
-    // 이는 거듭 제곱합 공식에 따라 최고 차항이 n^4이 됨
+    // return을 사용한 백트래킹이 없는 경우 아래와 같음
+    // O(n^3), n = s.length()
+    // 도출법 -> Σ(1 <= L <= n){ (n - L + 1) * L }
+    // (안쪽 for문) * (isPalindrome)
+    // 이는 거듭 제곱합 공식에 따라 최고 차항이 n^3이 됨
+    
+    // 백트랙킹을 사용한 경우는 따로 계산해봐야하는데 너무 복잡...
 }
+
+/* 다른 사람 코드, 시간복잡도 O(n^2), n = s.length()
+public int solution(String s) {
+    int left = 0, right = 0;
+    int result = 1;
+    if(s.length() != 1) return 1;
+    
+    // palindrome의 길이가 홀수일 때
+    for (int center = 1; center < s.length() - 1; center++) {
+        left = center - 1;
+        right = center + 1;
+        while (left >= 0 && right <= s.length() - 1) {
+            if (s.charAt(left) != s.charAt(right))
+                break;
+
+            result = right - left + 1 > result ? right - left + 1 : result;
+            left--;
+            right++;
+        }
+    }
+
+    // palindrome의 길이가 짝수일 때
+    for (int center = 0; center <= s.length() - 2; center++) {
+        left = center;
+        right = center + 1;
+        while (left >= 0 && right <= s.length() - 1) {
+            if (s.charAt(left) != s.charAt(right))
+                break;
+            result = right - left + 1 > result ? right - left + 1 : result;
+            left--;
+            right++;
+        }
+    }
+    
+
+    return result;
+}
+*/
