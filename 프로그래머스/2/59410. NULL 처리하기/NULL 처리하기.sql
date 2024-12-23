a22a3da7867c9d3964bd4b10d8b82707cbe42230
Heo SeokJin <pooh6195@naver.com>
2024-12-23 20:48:58 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,9 +1,6 @@
 -- 코드를 입력하세요
 SELECT
     ANIMAL_TYPE,
-    CASE
-        WHEN NAME IS NULL THEN 'No name'
-        ELSE NAME
-    END,
+    IFNULL(NAME, 'No name') AS NAME,
     SEX_UPON_INTAKE
 FROM ANIMAL_INS
